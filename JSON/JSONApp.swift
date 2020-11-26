b4dc7b6a462010b24ce4e646dd5772ca7efd0a19
Andreas Liebich <aliebich@lcs.on.ca>
2020-11-26 08:55:16 -0500

    Initial Commit

---
+++ JSON/JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONApp.swift
+//  JSON
+//
+//  Created by Andreas Liebich on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
